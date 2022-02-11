dbb0849ce568f64d1d14e464a4b8f56d942c5da1
Violet <violetmedina@gmail.com>
2022-02-10 20:25:16 -0600

    uploading HW for today

---
+++ hw-02-10-22.py
@@ -0,0 +1,17 @@
+
+
+#? Multiplication Table
+
+x = 1 
+
+while x < 11:
+    
+    y = 1
+    while y < 11:
+        z = x*y
+        print(f"{x} x {y} = {z}")
+        y += 1
+        
+    x+=1 
+
+
