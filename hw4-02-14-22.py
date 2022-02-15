808d7e3b3a4c2793a6525b904d131338a83b3b7c
Violet <violetmedina@gmail.com>
2022-02-14 20:44:04 -0600

    HW3

---
+++ hw4-02-14-22.py
@@ -0,0 +1,52 @@
+
+
+# 1. Find the smallest number
+# Write a function smallest that accepts a List of numbers as an argument.
+
+# nums = [1, 2, 3, 4, 5]
+# def smallest(nums):
+#     nums.sort()
+#     print(nums[:1])
+    
+# smallest(nums)
+
+# 2. Find the largest number
+# Write a function largest that accepts a List of numbers as an argument.
+# It should return the largest number in the List.
+
+# nums = [1, 2, 3, 4, 5]
+# def largest(nums):
+#     nums.sort()
+#     print(nums[-1:])
+
+# largest(nums)
+
+# 3. Find the shortest String
+# Write a function shortest that accepts a List of Strings as an argument.
+# It should return the shortest String in the List.
+
+# string = ["find", "shortest", "string", "fox"]
+
+# def shortest(some_string):
+#     new_string = some_string[0]
+#     for i in some_string:
+#         if len(i) < len(new_string):
+#             new_string = i
+#     print(new_string)
+
+# shortest(string)
+
+# 4. Find the longest String
+# Write a function longest that accepts a List of Strings as an argument.
+# It should return the longest String in the List.
+
+# string = ["find", "longest", "string", "fox"]
+
+# def longest(some_string):
+#     new_string = some_string[0]
+#     for i in some_string:
+#         if len(i) > len(new_string):
+#             new_string = i
+#     print(new_string)
+
+# longest(string)
